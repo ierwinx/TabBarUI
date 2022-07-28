4780716172723ddf1086b852db596949f3be50cc
Eяw!и Gontier <ierwinx@me.com>
2022-07-27 23:28:54 -0500

    Initial Commit

---
+++ TabBarUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabBarUI
+//
+//  Created by Erwin Luz León on 27/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabBarUI/TabBarUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarUIApp.swift
+//  TabBarUI
+//
+//  Created by Erwin Luz León on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TabBarUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
